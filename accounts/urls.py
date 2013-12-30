6c60376ca453b2ba6629c79307ca837f108b8384
Sharl <amoiz.shine@gmail.com>
2013-12-30 18:32:53 +0800

    add app key.
    Change-Id: I084f42394dc78422b8ba9a1a7fb0c152e14bd3b6
    Signed-off-by: Sharl <amoiz.shine@gmail.com>

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
+
 from accounts.views import Register
+from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Max
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
@@ -5,9 +6,16 @@ from django.views.decorators.http import require_http_methods
 from django.views.generic.base import TemplateView
 import random
 import string
+
 from accounts.models import Partner
 
+
 __author__ = 'sharl'
+
+_TYPE_GAME = 0
+_TYPE_MUSIC = 1
+_TYPE_LIVE = 2
+_TYPE_TRANSPORT = 3
 
 
 class Register(TemplateView):
@@ -24,13 +32,29 @@ def do_reg(request):
                       app_name=request.POST.get('appname'),
                       email=request.POST.get('email'),
                       notify_url=request.POST.get('notifyid'),
-                      app_id=gen_app_id()
+                      app_type=request.POST.get('apptype'),
+                      app_id=gen_app_id(request.POST.get('apptype')),
+                      app_key=gen_app_key(),
     )
     partner.save()
     return render_to_response("waitting.html")
 
+def gen_app_id(type):
+    max_id = Partner.objects.filter(app_type=type).aggregate(Max('app_id'))['app_id__max']
+    if max_id == 0:
+        if type == _TYPE_GAME:
+            max_id = 1000
+        elif type == _TYPE_MUSIC:
+            max_id == 2000
+        elif type == _TYPE_LIVE:
+            max_id == 3000
+        elif type == _TYPE_TRANSPORT:
+            max_id == 4000
+    else:
+        max_id += 1
+    return max_id
 
-def gen_app_id():
+def gen_app_key():
     strs = string.ascii_uppercase + string.ascii_lowercase + string.digits
     while True:
         id = "".join(random.sample(strs, 10))
@@ -42,7 +66,7 @@ def gen_app_id():
 @csrf_exempt
 @require_http_methods(['GET'])
 def manager(request):
-    partner = Partner.objects.filter(real=0)
+    partner = Partner.objects.filter()
     return render_to_response("manager.html", {"partner": partner})
 
 
